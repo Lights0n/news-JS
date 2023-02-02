b2f898d14bd56f6d5bb35bc2de2d9506d9a4c188
Lights0n <lyadouskiy@gmail.com>
2023-02-03 01:24:48 +0300

    feat: add styles, change apiKey

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,8 +2,9 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'e8dad77b87b9487a8f2c98ce99277333', // получите свой ключ https://newsapi.org/
+        // super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '18897f5f951843439cf63c625a5002f0', // получите свой ключ https://newsapi.org/
         });
     }
 }
